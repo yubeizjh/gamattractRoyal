2638217eec8844d159a61d7344730b64908eeaa2
yubeizjh <b00763378@essec.edu>
2022-03-30 23:57:53 +0200

    submit test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,59 @@
+<script>
+    import { supabase } from "../supabaseClient"
+
+async function getData() {
+  const { data, error } = await supabase
+    .from('testInfo')
+    .select()
+  if (error) throw new Error(error.message)
+  
+  return data
+}
+
+let newGame
+let submit = false
+
+async function sendData() {
+  const { data, error } = await supabase
+    .from('testInfo')
+    .insert([
+      { 'title': newGame }
+    ])
+  if (error) throw new Error(error.message) 
+  
+  return data
+}
+
+</script>
+
 <h1 class="text-blue-600">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<article class="prose lg:prose-xl">
-    <h1>Garlic bread with cheese: What the science tells us</h1>
-    <p>
-      For years parents have espoused the health benefits of eating garlic bread with cheese to their
-      children, with the food earning such an iconic status in our culture that kids will often dress
-      up as warm, cheesy loaf for Halloween.
-    </p>
-    <p>
-      But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-      springing up around the country.
-    </p>
-    <!-- ... -->
-</article>
+<h1>My favorite games</h1>
+
+{#await getData()}
+  <p>Fetching data...</p>
+{:then data}
+  {#each data as game}
+    <li>{game.title}</li>
+  {/each}
+{:catch error}
+  <p>Something went wrong while fetching the data:</p>
+  <pre>{error}</pre>
+{/await}
+
+<form on:submit|preventDefault={() => submit = true}>
+    <input type="text" bind:value={newGame}>
+    <input type="submit" value="Submit" on:click={() => submit = false}>
+</form>
+
+{#if submit}
+    {#await sendData()}
+      <p>Sending data...</p>
+    {:then data}
+      <p>Successfully sent data.</p>
+    {:catch error}
+      <p>Something went wrong while sending the data:</p>
+      <pre>{error}</pre>
+    {/await}
+{/if}
