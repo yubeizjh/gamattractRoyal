a7255873245d97f16f994767292930f8dacf3d07
yubeizjh <b00763378@essec.edu>
2022-04-27 12:41:06 +0800

    message layout

--- src/routes/message.svelte
+++ src/routes/message.svelte
@@ -1,13 +1,11 @@
 {#each meId as mid}
     <div class="w-5/6 lg:w-2/3 flex justify-center items-start m-auto my-16">
-        {#if mid%2 == 0}
         <div class="avatar">
             <div class="w-24 mask mask-squircle mr-5">
-                <img src="/stockImg/abzu-md.jpg" alt=""/>
+                <img src={picSrc[randomAvatarIndex()]} alt=""/>
             </div>
         </div>
-        {/if}
-        <div class="rounded-3xl w-full bg-blue-50">
+        <div class="rounded-3xl w-full bg-zinc-300">
             <div class="text-black m-5 mr-3">
                 <div class="flex items-end mb-3">
                     <p class="text-xl mr-3">{meName[mid]}</p>
@@ -16,18 +14,31 @@
                 <p>{meComment[mid]}</p>
             </div>
         </div>
-        {#if mid%2 == 1}
-        <div class="avatar">
-            <div class="w-24 mask mask-squircle ml-5">
-                <img src="/stockImg/abzu-md.jpg" alt=""/>
-            </div>
-        </div>
-        {/if}
     </div>
 {/each}
 
+<form class="text-white  w-5/6 lg:w-2/3 grid place-items-center m-auto my-16" 
+    on:submit|preventDefault={() => submit = true}>  
+    <input class="placeholder:text-zinc-400 placeholder:italic placeholder:text-center bg-zinc-600 rounded-2xl w-full mb-3 required" type="text" bind:value={inName} placeholder="昵称">
+    <textarea class="placeholder:text-zinc-400 placeholder:italic placeholder:text-center bg-zinc-600 rounded-2xl w-full mb-3 h-32 required" bind:value={inContent} placeholder="留言"></textarea>
+    <button class="rounded-full w-1/3 bg-zinc-500 hover:bg-zinc-700 text-white font-bold py-2 px-4" type="submit" value="Submit" on:click={() => submit = false}>
+        提交
+    </button>  
+</form>
+
 <script>
+    let inName
+    let inContent
+    let submit
+
     let meId = [0,1,2]
+    let picSrc = [
+        "/stockImg/abzu-md.jpg",
+        "/stockImg/acodyssey-md.jpg",
+        "/stockImg/Arceus-md.jpeg",
+        "/stockImg/bloodborne-md.jpg",
+        "/stockImg/control-md.jpg",
+    ]
     let meName = [
         "name1",
         "name2",
@@ -43,6 +54,11 @@
         "wxl是果蝇。wxl是果蝇。wxl是果蝇。wxl是果蝇。wxl是果蝇。",
         "你才是果蝇。"
     ]
+
+    function randomAvatarIndex(){
+        return Math.floor(Math.random() * picSrc.length)
+    }
+
 </script>
 
 
