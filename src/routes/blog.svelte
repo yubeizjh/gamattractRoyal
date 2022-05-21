1a370fb7705425704b9cb64f9cb0576620627d65
yubeizjh <b00763378@essec.edu>
2022-05-22 03:13:58 +0800

    blog新增筛选

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,13 +1,29 @@
-<div class="flex justify-center btn-group my-5">
-    {#each pageButton as item}
-        {#if item == pageMain}
-            <button class="btn px-8 bg-zinc-200 text-black hover:bg-zinc-200 hover:text-black">{item}</button>
-        {:else}
-            <button class="btn px-8 text-zinc-200 bg-zinc-900 hover:bg-black" 
-            on:click={() => changePageMain(item)}>{item}</button>
-        {/if}
-    {/each}
-  </div>
+<div class="flex justify-center">
+
+{#if enableFilter}
+    <div class="flex justify-center btn-group my-5">
+        {#each pageButton as item}
+            {#if item == pageMain}
+                <button class="whitespace-nowrap btn px-8 bg-zinc-200 text-black hover:bg-zinc-200 hover:text-black">{item}</button>
+            {:else}
+                <button class="whitespace-nowrap btn px-8 text-zinc-200 bg-zinc-900 hover:bg-black" 
+                on:click={() => changePageMain(item)}>{item}</button>
+            {/if}
+        {/each}
+    </div>
+{/if}
+
+<div class="flex justify-center my-5 ml-5">
+    <button class="whitespace-nowrap btn px-8 bg-zinc-900 text-white hover:bg-zinc-200 hover:text-black" on:click={changeFilter}>
+      {#if !enableFilter}
+        开启筛选
+      {:else}
+        关闭筛选
+      {/if}
+    </button>
+</div>
+
+</div>
 
 <div class="w-5/6 m-auto md:w-2/3 lg:w-1/2">
 {#if refreshTrigger}
@@ -39,15 +55,28 @@ import { supabase } from "../supabaseClient"
 let pageButton = ['综合','单品']
 let pageMain = pageButton[0]
 
+let enableFilter = false;
+
+function changeFilter(){
+    enableFilter = !enableFilter
+    infoRefresh();
+}
+
 async function getBlogs () {
-  const { data , error } = await supabase
-      .from ('blog')
-      .select ()
-      .eq ('ready',true)
-      .eq ('forGame',getCurrentChoice())
-      .order('id', { ascending: false })
-  if (error) throw new Error(error.message)
-  return data 
+
+    let query = supabase
+        .from ('blog')
+        .select ()
+        .eq ('ready',true)
+        .order('id', { ascending: false })
+
+    if(enableFilter)
+        query = query.eq ('forGame',getCurrentChoice())
+
+    const { data , error } = await query
+
+    if (error) throw new Error(error.message)
+    return data 
 }
 
 function getCurrentChoice(){
